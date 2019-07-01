825df89b65e97426b5b05fac57868ef04db0b47c
SeungheonOh <dan.oh0721@gmail.com>
2019-07-01 11:08:15 -0500

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -4,23 +4,6 @@
 
 
 int main(){
-  std::cout << EmojiTools::countEmoji("😄 😆 😊 😃 ☺️");
+  std::cout << EmojiTools::countEmoji("Hello World!😄");
   return 0;
-  /*
-  std::ifstream input;
-  std::ofstream output;
-  input.open("input.txt");
-  output.open("output.txt");
-  std::string line;
-  while(std::getline(input, line)){
-    std::string s = emojicpp::emojize(line);
-    unsigned int sum = 0;
-    if(s.length() > 4) continue;
-    for(int i = 0; i < s.length(); i++){
-      sum += (unsigned int)(unsigned char)s[i] << 8 * (3 - i);
-    }
-    output << std::hex << sum << "/" << line << std::endl;
-  }
-  return 0;
-  */
 }
