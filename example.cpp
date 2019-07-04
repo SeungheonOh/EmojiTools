ba9eeb92d1a994c6fd4369ae435655404be235e9
DanielOh <dan.oh0721@gmail.com>
2019-07-03 22:25:38 -0500

    time checker

--- example.cpp
+++ example.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <string>
+#include <chrono>
 #include "EmojiTools.h"
 
 
 int main(){
+  auto start = std::chrono::high_resolution_clock::now();
   std::string a = "Hello World!😄";
   std::cout << EmojiTools::deEmojize(a) << std::endl;
   std::cout << EmojiTools::deEmojize<char>(a, '{', '}') << std::endl;
   std::cout << "Numbers of emojis : " << EmojiTools::countEmoji(a) << std::endl;
   std::cout << EmojiTools::getEmoji("thumbsup") << std::endl;
   std::cout << EmojiTools::emojize(":thumbsup: Hello world!") << std::endl;
+  auto finish = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = finish - start;
+  std::cout << "Elapsed time: " << elapsed.count() << " s\n";
   return 0;
 }
